2beb9fee9f9a5bf101296bcd2d6c9e1a1bd990b2
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-02-16 22:35:56 +0530

    Create checkPangram.cpp

---
+++ checkPangram.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool checkPangram(string str){
+      bool count[26]={false};
+      bool pang = true;
+      for(int i=0 ;i<str.length() ;i++){
+
+          if(str[i]>='a' and str[i]<='z' ){
+              count[str[i]-'a'] =true;
+          }
+          else if(str[i]>='A' and str[i]<='Z'){
+              count[str[i]-'A'] =true;;
+          }
+      }
+
+      for(int i=0 ;i<26 ;i++){
+          if(!count[i]){
+              pang =false;
+              break;
+          }
+      }
+      return pang;
+}
+
+int main(){
+
+    string str;
+    cout<<"enter a word: ";
+    getline(cin ,str);
+
+    if(checkPangram(str)){
+        cout<<str<<" is a pangram";
+    }
+    else{
+        cout<<str<<" is not a pangram";
+    }
+
+    return 0;
+}
