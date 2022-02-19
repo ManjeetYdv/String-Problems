0093ffc62f8f68d071fb356456fa1661591c3bb1
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-02-19 09:07:16 +0530

    Add files via upload

---
+++ distinctSubsequence.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int distinct(string str){
+   
+  
+   int arr[str.length()+1];
+   arr[0]=1;
+ 
+   for(int i=0 ;i<str.length() ;i++){
+     
+      if(str.find(str[i])!=i )
+      
+       {  int j;
+         // arr[i+1]=arr[i]*2 - arr[str.find(str[i])];
+         for(j= i-1 ;j>=0 ;j--){
+            if(str[j]==str[i]){
+               break;
+            }
+         }
+            arr[i+1]= arr[i]*2 - arr[j];
+
+         }
+   
+       else if(str.find(str[i])==i){
+          arr[i+1] = arr[i]*2;
+       }
+
+   }
+   
+ 
+
+    return arr[str.length()];
+}
+
+
+int main(){
+   
+   string str="ggg";
+   int dis = distinct(str);
+   
+   cout<<"Number of distinct subsequence "<<str<<" is: "<<dis;
+
+
+   return 0;
+}
