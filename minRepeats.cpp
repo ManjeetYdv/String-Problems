eb5b61fc97f0463dd9a2e8966764245add568bd4
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-03-08 00:25:45 +0530

    Create minRepeats.cpp
    Given two strings A and B. Find minimum number of times A has to be repeated such that B is a Substring of it. If B can never be a substring then return -1.

---
+++ minRepeats.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int minRepeat(string s1 , string s2){
+    
+    if(s1.find(s2)>=0 and s1.find(s2)<s1.length()){
+        return 1;
+    } 
+
+    int a = s1.length();
+    int b = s2.length();
+    int k =  b/a;
+
+    for(int i=0 ; i<k ;i++){
+      s1+= s1.substr(0 , a);
+      
+
+
+
+      if(s1.find(s2)>=0 and s1.find(s2)<s1.length()){
+        
+          return i+2;
+      }
+
+    }
+
+return -1;
+}
+
+int main(){
+ 
+
+ string str1 ="abc";
+ string str2 ="cabc";
+
+ cout<<minRepeat(str1 , str2);
+    
+}
