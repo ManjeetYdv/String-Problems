3a981c7643896aeb06a52e5b735754237b565e25
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-07-18 14:18:14 +0530

    Update minDeletion_To_Make_String_Palindrome.cpp

--- minDeletion_To_Make_String_Palindrome.cpp
+++ minDeletion_To_Make_String_Palindrome.cpp
@@ -1,3 +1,5 @@
+
+//Recursion 
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -32,12 +34,26 @@ int minimumNumberOfDeletions(string str){
 }
 
 
-int main(){
-   string str="aebcbda";
-   
-   int minDelete =minimumNumberOfDeletions(str);
+// Optimized using dp
 
-   cout<<"Minimum number of deletion to make "<<str<<" a palindrome string is: "<<minDelete;
-
-
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+         
+        int[][]dp = new int[text1.length()+1][text2.length()+1];
+        
+        for(int i=1 ;i<=text1.length() ;i++){
+            for(int j=1; j<=text2.length() ;j++){
+               
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1+ dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+                }
+                
+            }
+        }
+        
+        return dp[text1.length()][text2.length()];
+    }
 }
